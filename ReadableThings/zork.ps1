c7423f18cf7e7685d0aec607daec844a8b76d6ea
unknown <c-secrej@PC9149.GMCC.GRANGE.LOCAL>
2012-10-05 15:55:38 -0400

    stuff

---
+++ ReadableThings/zork.ps1
@@ -0,0 +1,42 @@
+﻿Push-Location
+cd "C:\Users\c-secrej\Desktop\Dungeon\source\ReadableThings\ReadableThings\bin\debug"
+Import-Module .\ReadableThings.dll
+Pop-Location
+
+function look
+{
+    Write-Host "There is nothing to see here." 
+}
+
+$defaultRoom = New-Object Ogresoft.Thing("obsure place")
+
+if ($you -eq $null)  {
+    $you = New-Object Ogresoft.Thing($env:USERNAME); 
+}
+
+$you.Move($defaultRoom)
+
+function create
+{
+    $description = ""
+    $args | foreach { $description = $description + " " + $_ }
+
+    if ([string]::IsNullOrEmpty($description)) { 
+        Write-Host "What is it that you wish to create?"
+        return $null;
+    }
+
+    $thing = New-Object Ogresoft.Thing($description)
+
+    if ($you -eq $null)  {
+        $you = New-Object Ogresoft.Thing($env:USERNAME);     
+    }
+
+    $thing.Move($you); 
+
+    return $thing 
+}
+
+function inventory {
+    Write-Host $you.Inventory.Description
+}
