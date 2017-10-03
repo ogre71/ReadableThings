1dbee09069774d7315922fb17c5e7b1fa3e4f757
ogre71 <ogre71@gmail.com>
2017-10-03 01:39:11 -0400

    Add adminthing

---
+++ Ogresoft.Parser/AdminThing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogresoft.Parser
+{
+    public class AdminThing : Thing
+    {
+        public AdminThing(string name) : base(name)
+        {
+            this.AddVerb("look", new Func<bool>(() => {
+                Messages.Action("{N0} {v0look} around.");
+                return true;
+            }));
+        }
+    }
+}
---
+++ Ogresoft.Parser/Repl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ogresoft;
+
+namespace Ogresoft.Parser
+{
+        public class Repl
+        {
+
+            public Repl()
+            {
+
+            }
+
+            public void Parse(string input, Thing doer)
+            {
+                string[] words = input.Split(' ');
+
+                if (doer.AllowUseAlone(words[0]))
+                {
+                    return;
+                }
+
+                doer.Tell(Messages.Nonsense());
+                // Assume first word is the verb
+                //if (doer.)
+            }//
+        }
+}
